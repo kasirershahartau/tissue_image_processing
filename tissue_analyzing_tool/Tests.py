ca8bd04e0536401a53c4c6fcf171b3cdd02498cc
aivanov <anastasiasofi@gmail.com>
2021-08-10 11:09:11 +0300

    Updating tests

--- tissue_analyzing_tool/Tests.py
+++ tissue_analyzing_tool/Tests.py
@@ -9,9 +9,11 @@ Small tests to debug image processing code
 from basic_image_manipulations import *
 import os
 from matplotlib import pyplot as plt
+from tissue_info import *
 
 test_dir = "D:\\Anastasia\\Fixed_samples"
-test_file = "Local Z Projection of 21_03_04_E17_utricle_Airyscan Processing_Stitch.tif"
+# test_file = "Local Z Projection of 21_03_04_E17_utricle_Airyscan Processing_Stitch.tif"
+test_file= "testimage.tif"
 test_path = os.path.join(test_dir, test_file)
 
 
@@ -26,9 +28,37 @@ def adjust_and_save(path):
 
 def loading_test_image():
      image, axes, shape, metadata = read_tiff(test_path)
-     adjusted = set_channel_brightness(image[0], 65535)
+     adjusted = set_brightness(image, axes)
      return adjusted
 
+def draw_neighbors(tissue):
+     for cell in test_tissue.cells.values():
+         centroidy, centroidx = cell.get_centroid()
+         for neighbor_label in cell.neighbors:
+             neighbor = test_tissue.get_cell(neighbor_label)
+             neighbor_centroidy, neighbor_centroidx = neighbor.get_centroid()
+             plt.plot((centroidx, neighbor_centroidx), (centroidy, neighbor_centroidy), linewidth = .3, color= 'blue')
+def add_numbers(ax, tissue):
+    
+    for label in tissue.cells.keys():
+         cell = tissue.get_cell(label) 
+         centroidy, centroidx = cell.get_centroid() 
+         ax.text(centroidx, centroidy, str(label), fontsize = 5)
+         
+     
 if __name__ == "__main__":
-     pass
- 
+    image = loading_test_image()
+    skeleton, segmented = watershed_segmentation(image[1], .6, 101, 5)
+    test_tissue = Tissue(segmented)
+    test_tissue.calculate_cellinfo()
+    
+    fig, ax = plt.subplots(1)
+    seg_copy = np.copy(segmented)
+    seg_copy[seg_copy > 0] = 1
+    ax.imshow(seg_copy, cmap='gray')
+    
+    test_tissue.get_cell_types(image[0])
+    #test_tissue.plot_cell_types()
+    #draw_neighbors(test_tissue)
+    
+    
