1a0b94f17359b1f8787827922a4f0444caaf5691
Shahar Kasirer <kasirershahar@gmail.com>
2021-08-10 10:45:44 +0300

    Updating tests for surface projection

--- tissue_analyzing_tool/Tests.py
+++ tissue_analyzing_tool/Tests.py
@@ -7,6 +7,7 @@ Created on Wed Jun  9 17:26:22 2021
 Small tests to debug image processing code
 """
 from basic_image_manipulations import *
+from surface_projection import *
 import os
 from matplotlib import pyplot as plt
 
@@ -31,8 +32,25 @@ def loading_test_image():
      return adjusted
 
 if __name__ == "__main__": 
-    i=0
-    for chunk in read_image_in_chunks(test_path, dx = 1000, dy = 1000, dz = 0, dc = 0, dt= 1):
-        chunk = set_brightness(chunk, "TCZXY", clearExtreamPrecentage=0)
-        save_tiff(os.path.join(test_dir, "chunk%d.tif"%i), (chunk*255).astype(np.uint8))
-        i+=1
+    dims = get_image_dimensions(test_path)
+    projection = np.zeros((3, dims.X, dims.Y))
+    x = 0
+    y = 0
+    dx = 1000
+    dy = 1000
+    for chunk in read_image_in_chunks(test_path, dx = dx, dy = dy, dz = 0, dc = 0, dt= 1):
+        chunk = set_brightness(chunk, "TCZXY", clearExtreamPrecentage=1)
+        chunk_x = min(x+dx, dims.X) - x
+        chunk_y = min(y+dy, dims.Y) - y
+        chunk_projection = surface_projection(chunk.reshape((3, 20, chunk_x, chunk_y)), "CZXY", 1, 0, 4, "max_std", 5)
+        projection[:,x:min(x+dx, dims.X), y:min(y+dy, dims.Y)] = chunk_projection
+        if x+dx > dims.X:
+            x = 0
+        if y+dy > dims.Y:
+            y = 0
+            x += dx
+        y += dy
+        
+    save_tiff(test_path.replace(".czi","_projected.tif"), projection, axes="CXY", data_type="uint16") 
+
+        
